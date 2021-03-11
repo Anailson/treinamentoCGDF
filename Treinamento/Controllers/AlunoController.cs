11336f739f368da161195801bbcd2aad79d5dddf
ANAÍLSON RIBEIRO <arsantos315@gmail.com>
2021-03-11 14:22:27 -0300

    01AulaAlunoControler

---
+++ Treinamento/Controllers/AlunoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Treinamento.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            /*CRIANDO ARRAY*/
+            string[] alunos =
+            {
+                "José",
+                "João",
+                "maria"
+
+            };
+            return View(alunos);
+        }
+    }
+}
